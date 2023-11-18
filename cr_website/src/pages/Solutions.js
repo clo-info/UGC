ea074d7b305fd9f2d3145a727fc082978c18ac6d
clo-info <71102522+clo-info@users.noreply.github.com>
2023-11-17 22:27:32 -0700

    had to stop to move desk

--- cr_website/src/pages/Solutions.js
+++ cr_website/src/pages/Solutions.js
@@ -4,19 +4,19 @@ function Solutions() {
     return (
         <div className='SolutionsPage'>
             <h1 className='SolutionPreamble'>The Betterment of Climate Refugees Everywhere Depends on <span style={{color: 'rgb(88, 55, 235)'}}>Three</span> Things...</h1>
-            <div className='Solution1'>
-                <h2 className='SolutionTitle'>The Good Shit</h2>
+            <div className='Solution'>
+                <h2 className='SolutionTitle' style={{color: 'rgb(88, 55, 235)'}}>Refugee Status for those Displaced by Climate</h2>
                     <h3 className='SolutionBody'>
                         Short para about why its good
                     </h3>
             </div>
-            <div className='Solution2'>
+            <div className='Solution'>
                 <h2 className='SolutionTitle'>The Good Shit</h2>
-                    <h3 className='SolutionBody'>
+                    <h3 className='SolutionBody' >
                         Short para about why its good
                     </h3>
             </div>
-            <div className='Solution3'>
+            <div className='Solution'>
                 <h2 className='SolutionTitle'>The Good Shit</h2>
                     <h3 className='SolutionBody'>
                         Short para about why its good
